9f1250cefc60ce939bda669663f3110c479e7dba
Greyy <rakhasaputra7g@gmail.com>
2025-05-11 11:10:41 +0700

    update hapus tugas

--- todolist.py
+++ todolist.py
@@ -3,10 +3,13 @@ todolistdata = []
 def todolist(data):
     if not data:
         return []
+
     result = []
+
     for idx, item in enumerate(data, start=1):
         status = "Selesai" if item["selesai"] == "selesai" else "belum selesai"
         result.append(f"{idx}. {item['tugas']} ({status})")
+
     return result
 
 def inputuser():
@@ -20,9 +23,11 @@ def ubahstatus(data):
         print("To Do list masih kosong")
         print()
         return
+
     for item in todolist(data):
         print()
         print(item)
+
     try:
         pilihan = int(input("Masukkan nomor tugas: "))
         if 1 <= pilihan <= len(data):
@@ -39,6 +44,27 @@ def ubahstatus(data):
         print("Input Harus Berupa Angka")
         print()
 
+def hapus(data):
+    if not data:
+        print()
+        print("To Do list masih kosong")
+
+    for item in todolist(data):
+        print()
+        print(item)
+
+    try:
+        pilihan = int(input("Masukkan Nomor Tugas Yang Akan Dihapus: "))
+        if 1 <= pilihan <= len(data):
+            data.pop(pilihan - 1)
+            print("Tugas Berhasil Dihapus")
+        else:
+            print("Nomor Tidak Valid")
+    except ValueError:
+        print()
+        print("Input Harus Berupa Angka")
+
+
 while True:
     print()
     print()
@@ -53,12 +79,15 @@ while True:
     print()
     print("1. Tambahkan Tugas")
     print("2. Ubah Status Tugas")
-    user = input("Pilih Perintah (1/2): ")
+    print("3. Hapus Tugas")
 
+    user = input("Pilih Perintah (1/2/3): ")
     if user == "1":
         inputuser()
     elif user == "2":
         ubahstatus(todolistdata)
+    elif user == "3":
+        hapus(todolistdata)
     else:
         print()
         print("Pilihan Tidak Tersedia")
